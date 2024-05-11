36e3ce3c87c4207142d820b2412e42875a2de82b
mazin <106538834+shoufan@users.noreply.github.com>
2024-05-11 14:45:29 -0500

    Login Component (UI)

---
+++ chat-app/components/Login.js
@@ -0,0 +1,37 @@
+import Head from "next/head";
+import {ChatIcon} from "@chakra-ui/icons"
+import { Box, Button, Center, Stack } from "@chakra-ui/react";
+
+export default function Login(){
+    return (
+        <>
+            <Head>
+            <title>Yokosou!</title>
+            </Head>
+
+            <Center
+            height={"100vh"}>
+                <Stack
+                align={"center"}
+                bgColor={"#6A4FD6"}
+                p={16}
+                rounded={"3xl"}
+                spacing={12}
+                boxShadow={"lg"}>
+                    <Box 
+                    bgColor={"#FC4778"}
+                    width={"fit-content"}
+                    padding={5}
+                    rounded={"3xl"}
+                    boxShadow={"md"}>
+                        <ChatIcon w = "100px" h = "100px" color = "#3952F5"/>
+                    </Box>
+
+                    <Button boxShadow={"md"} bgColor={"#FC4778"} textColor={"white"}>Sign in with Google</Button>
+                </Stack>
+            </Center>
+
+        </>
+        
+    )
+}
--- chat-app/pages/_app.js
+++ chat-app/pages/_app.js
@@ -1,4 +1,13 @@
+import { ChakraProvider } from "@chakra-ui/react"
+import Login from "../components/Login.js"
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  // return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Login />
+    </ChakraProvider>
+    
+  )
 }
 
